42b7710f783f262a27452da153012269de0acdba
Vilho Kn <73425056+VilhoKn@users.noreply.github.com>
2023-04-27 08:34:53 +0000

    added styling

--- script.js
+++ script.js
@@ -19,16 +19,21 @@ const emptySave = (x, y) => {
 }
 
 const init = () => {
-    for (const row of save) {
+    document.querySelector(".grid-container").innerHTML = ""
+    console.log(myObj)
+    for (let i = 0; i < myObj.data.length; i++) {
         let rowEl = document.createElement("div")
         rowEl.classList.add("row")
-        for (const pixel of row) {
+        for (let j = 0; j < myObj.data[i].length; j++) {
             let pixelEl = document.createElement("div")
             pixelEl.addEventListener("click", () => {
-                
+                myObj.data[i][j] = [myObj.var[color].rgb]
+                pixelEl.style.background = `rgb(${myObj.var[color].rgb.replace(" ", ",")})`
+                console.log(myObj)
+                init()
             })
             pixelEl.classList.add("pixel")
-            pixelEl.style.background = `rgb(${pixel.join(",")})`
+            pixelEl.style.background = `rgb(${myObj.data[i][j].join(",")})`
             rowEl.appendChild(pixelEl)
         }
         document.querySelector(".grid-container").appendChild(rowEl)
@@ -39,3 +44,6 @@ if (save.length == 0) {
     save = emptySave(10, 10)
     myObj.data = save
 }
+
+init()
+console.log(myObj)
